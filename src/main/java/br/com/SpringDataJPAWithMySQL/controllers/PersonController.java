36863eb6df9dd7e5b32ccaf1a591b4a376dda132
William Santana <williamandrade1058@gmail.com>
2025-06-24 21:38:52 -0300

    Criando a Base de Dados e Definindo as Propriedades de Conexão

---
+++ src/main/java/br/com/SpringDataJPAWithMySQL/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package br.com.SpringDataJPAWithMySQL.controllers;
+
+import br.com.SpringDataJPAWithMySQL.model.Person;
+import br.com.SpringDataJPAWithMySQL.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public List<Person> findAll() {
+        return service.findByAll();
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
+    }
+}
---
+++ src/main/java/br/com/SpringDataJPAWithMySQL/services/PersonServices.java
@@ -0,0 +1,65 @@
+package br.com.SpringDataJPAWithMySQL.services;
+
+import br.com.SpringDataJPAWithMySQL.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("William");
+        person.setLastName("Santana");
+        person.setAddress("Aracaju - Sergipe - Brazil");
+        person.setGender("Male");
+
+        return person;
+    }
+
+    public List<Person> findByAll() {
+        logger.info("Find All People");
+
+        List<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Update one person!");
+        return person;
+    }
+
+    public void delete(String id) {
+        logger.info("Deleting one person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Firstname: " + i);
+        person.setLastName("Lastname: " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male");
+        return person;
+    }
+}
