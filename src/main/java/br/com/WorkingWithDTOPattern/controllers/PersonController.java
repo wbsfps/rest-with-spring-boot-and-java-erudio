f6f40b644d6aacdeba6972a20aaa90c3733fdcc9
William Santana <williamandrade1058@gmail.com>
2025-06-26 13:51:22 -0300

    Ajustando o Service e o Controller

--- src/main/java/br/com/WorkingWithDTOPattern/controllers/PersonController.java
+++ src/main/java/br/com/WorkingWithDTOPattern/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package br.com.WorkingWithDTOPattern.controllers;
 
-import br.com.WorkingWithDTOPattern.model.Person;
+import br.com.WorkingWithDTOPattern.data.dto.PersonDTO;
 import br.com.WorkingWithDTOPattern.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,22 +17,22 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         return service.findByAll();
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
--- src/main/java/br/com/WorkingWithDTOPattern/services/PersonServices.java
+++ src/main/java/br/com/WorkingWithDTOPattern/services/PersonServices.java
@@ -1,6 +1,10 @@
 package br.com.WorkingWithDTOPattern.services;
 
+import br.com.WorkingWithDTOPattern.data.dto.PersonDTO;
 import br.com.WorkingWithDTOPattern.exception.ResourceNotFoundException;
+import static br.com.WorkingWithDTOPattern.mapper.ObjectMapper.parseListObjects;
+import static br.com.WorkingWithDTOPattern.mapper.ObjectMapper.parseObject;
+
 import br.com.WorkingWithDTOPattern.model.Person;
 import br.com.WorkingWithDTOPattern.repository.PersonRepository;
 import org.slf4j.Logger;
@@ -19,23 +23,26 @@ public class PersonServices {
     @Autowired
     private PersonRepository repository;
 
-    public Person findById(Long id) {
+    public PersonDTO findById(Long id) {
         logger.info("Finding one Person!");
 
-        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records not found this ID"));
+        var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records not found this ID"));
+        return parseObject(entity, PersonDTO.class);
     }
 
-    public List<Person> findByAll() {
+    public List<PersonDTO> findByAll() {
         logger.info("Find All People");
-        return repository.findAll();
+        return parseListObjects(repository.findAll(), PersonDTO.class);
     }
 
-    public Person create(Person person) {
+    public PersonDTO create(PersonDTO person) {
         logger.info("Creating one person!");
-        return repository.save(person);
+        var entity = parseObject(person, Person.class);
+
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO person) {
         logger.info("Update one person!");
 
         var entity = repository.findById(person.getId())
@@ -46,7 +53,7 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
 
-        return repository.save(entity);
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
     public void delete(Long id) {
