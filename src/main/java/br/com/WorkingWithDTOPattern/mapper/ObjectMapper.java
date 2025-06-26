8045ff15a38182ca4e46d275810133e51b0d65bb
William Santana <williamandrade1058@gmail.com>
2025-06-26 13:30:19 -0300

    Adicionando os Testes do Mapper de Entidate e DTO

--- src/main/java/br/com/WorkingWithDTOPattern/mapper/ObjectMapper.java
+++ src/main/java/br/com/WorkingWithDTOPattern/mapper/ObjectMapper.java
@@ -12,7 +12,7 @@ public class ObjectMapper {
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
     }
-    public static <O, D> List<D> parseObject(List<O> origin, Class<D> destination) {
+    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
         List<D> destinationObjects = new ArrayList<>();
         for (Object o : origin) {
             destinationObjects.add(mapper.map(o, destination));
